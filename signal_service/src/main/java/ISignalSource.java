6e673ce52fbe8b395a457a9eea38d3672089b885
BadCuzMad <72146849+BadCuzMad@users.noreply.github.com>
2021-08-27 14:07:30 +0300

    Delete signal_service directory

--- signal_service/src/main/java/ISignalSource.java
+++
@@ -1,8 +0,0 @@
-public interface ISignalSource {
-    Object CreateSignal(Object ampl, Object ffd, Object freq);
-    double GetSignal(int isp, int iip);
-    String asString();
-    MainSignals.TQuads GetQuads();
-    double GetAmpl();
-    void SetAmpl (double ampl);
-}
